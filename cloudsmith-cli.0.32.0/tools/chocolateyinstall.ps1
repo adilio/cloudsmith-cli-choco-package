4a048f5f5f5df9279643721c96b388b0aa1f0d0a
Adil Leghari <adilio@gmail.com>
2022-03-12 14:13:29 -0800

    initial commit with source files

---
+++ cloudsmith-cli.0.32.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$Python = (Get-ChildItem C:\python* -Recurse -Filter python.exe).FullName[-1]
+Start-ChocolateyProcessAsAdmin -Statements "-m pip install --upgrade pip" -ExeToRun $Python
+$Pip = (Get-ChildItem C:\python*\ -Recurse -Filter pip.exe).Fullname
+Start-ChocolateyProcessAsAdmin -Statements "install --upgrade cloudsmith-cli==0.32.0" -ExeToRun $Pip
+refreshenv
+Write-Host -ForegroundColor Green "The cloudsmith-cli package has now been installed successfully."s
---
+++ cloudsmith-cli.0.32.0/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'
+$ExeToRun = (Get-ChildItem C:\python*\ -Recurse -Filter pip.exe).Fullname
+Start-ChocolateyProcessAsAdmin -Statements "uninstall cloudsmith-cli -y" -ExeToRun $ExeToRun
