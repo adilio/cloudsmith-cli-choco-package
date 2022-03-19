ec2b41b046ac600c5916a072d617a82dacea65fd
Adil Leghari <adilio@gmail.com>
2022-03-19 00:06:38 -0700

    Removed about section in nuspec, and refreshenv

--- cloudsmith-cli.0.32.0/tools/chocolateyinstall.ps1
+++ cloudsmith-cli.0.32.0/tools/chocolateyinstall.ps1
@@ -4,5 +4,8 @@ $Python = (Get-ChildItem C:\python* -Recurse -Filter python.exe).FullName[-1]
 Start-ChocolateyProcessAsAdmin -Statements "-m pip install --upgrade pip" -ExeToRun $Python
 $Pip = (Get-ChildItem C:\python*\ -Recurse -Filter pip.exe).Fullname
 Start-ChocolateyProcessAsAdmin -Statements "install --upgrade cloudsmith-cli==0.32.0" -ExeToRun $Pip
-refreshenv
-Write-Host -ForegroundColor Green "The cloudsmith-cli package has now been installed successfully."
+$Warn = @"
+WARNING: You will likely need to close and repoen your console, or run 'refreshenv',
+before the 'cloudsmith' commands are available.
+"@
+Write-Host -ForegroundColor Magenta $Warn
